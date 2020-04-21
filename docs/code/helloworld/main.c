95cf3dc910749d1f4d73f6cbb03bc0b5ec05e069
evan <pingplus@sohu.com>
2020-04-21 11:48:27 +0800

    try uv_default_loop()

--- docs/code/helloworld/main.c
+++ docs/code/helloworld/main.c
@@ -3,13 +3,17 @@
 #include <uv.h>
 
 int main() {
-    uv_loop_t *loop = malloc(sizeof(uv_loop_t));
-    uv_loop_init(loop);
+//    uv_loop_t *loop = malloc(sizeof(uv_loop_t));
+//    uv_loop_init(loop);
+//    printf("Now quitting.\n");
+//    uv_run(loop, UV_RUN_DEFAULT);
+//    uv_loop_close(loop);
+//    free(loop);
 
+    uv_loop_t *loop = uv_default_loop();
     printf("Now quitting.\n");
     uv_run(loop, UV_RUN_DEFAULT);
-
     uv_loop_close(loop);
-    free(loop);
+    
     return 0;
 }
