9ddff00168bf174ce67b9331307b2767a2a0dba0
evan <pingplus@sohu.com>
2020-04-23 09:46:03 +0800

    fix bug in client.c : nbufs set wrong when invoke uv_write

--- docs/code/tcp-echo-server/client.c
+++ docs/code/tcp-echo-server/client.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #define BUF_SIZE 128
+#define DEFAULT_PORT 7000
 
 typedef struct {
     uv_write_t req;
@@ -11,7 +12,7 @@ typedef struct {
 } write_request_t;
 
 void connect_cb(uv_connect_t* req, int status);
-void close_cb(uv_handle_t* handle);
+// void close_cb(uv_handle_t* handle);
 void write_cb(uv_write_t* req, int status);
 void alloc_cb(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf);
 void read_cb(uv_stream_t* stream, ssize_t nread, const uv_buf_t* buf);
@@ -23,8 +24,11 @@ int main(int argc, char **argv) {
     struct sockaddr_in dest_addr;
 
     uv_tcp_init(loop, &sock);
-    uv_connect_t *connect_req = (uv_connect_t *)malloc(sizeof(uv_connect_t));
-    uv_ip4_addr(argv[1], atoi(argv[2]), &dest_addr);
+    // uv_connect_t *connect_req = (uv_connect_t *)malloc(sizeof(uv_connect_t)); // main 退出时自动释放 connect_req，所以不需要用malloc 分配内存
+    uv_connect_t *connect_req;
+
+    // uv_ip4_addr(argv[1], atoi(argv[2]), &dest_addr);
+    uv_ip4_addr("127.0.0.1", DEFAULT_PORT, &dest_addr);
     uv_tcp_connect(connect_req, &sock, (const struct sockaddr *)&dest_addr, connect_cb);
 
     uv_run(loop, UV_RUN_DEFAULT);
@@ -33,9 +37,10 @@ int main(int argc, char **argv) {
 void connect_cb(uv_connect_t* connect_req, int status) {
     if (status < 0) { // error!
         fprintf(stderr, "Connection error: %s\n", uv_strerror(status));
-        
-        uv_close((uv_handle_t*) connect_req->handle, close_cb);
-        free(connect_req);
+
+        uv_close((uv_handle_t*) connect_req->handle, 0); // 没资源需要回收，不需要注册 callback
+        // free(connect_req); // connect_req 在 main 中是局部变量，不需要回收
+
         return;
     }
 
@@ -44,24 +49,28 @@ void connect_cb(uv_connect_t* connect_req, int status) {
     input_and_send((uv_stream_t *)connect_req->handle);
 }
 
+/**
+ * 注意：每次进入 input_and_send 时，都会为 write_req，write_req->buf.base (write_req->buf.base = input_buf) 分配内存，
+ *      所以在 write_cb() 的回调时，需要回收这2个数据的内存。
+ */
 void input_and_send(uv_stream_t * handle) {
     char *input_buf = (char *)malloc(BUF_SIZE);
+
     memset(input_buf, 0, BUF_SIZE);
-    printf("Input now: ");
     if(fgets(input_buf, BUF_SIZE, stdin)){
         input_buf[strlen(input_buf) - 1] = 0;
     }
-    puts("Sendto server ! ");
 
     write_request_t *write_req = (write_request_t *)malloc(sizeof(write_request_t));
-    write_req->buf = uv_buf_init(input_buf, strlen(input_buf));
+    write_req->buf = uv_buf_init(input_buf, strlen(input_buf)); // input_buf 被挂到 write_req->buf.base 上了
+    input_buf = 0;
 
-    uv_write((uv_write_t *)write_req, handle, &write_req->buf, sizeof(write_req->buf), write_cb);
+    uv_write((uv_write_t *)write_req, handle, &write_req->buf, 1, write_cb);
 }
 
-void close_cb(uv_handle_t* sock) {
-    free(sock);
-}
+// void close_cb(uv_handle_t* sock) {
+//     // free(sock); // sock 是在 main(）中声明的局部变量，不需要释放
+// }
 
 void write_cb(uv_write_t* wr_req, int status) {
     if (status) { // status will be 0 in case of success, < 0 otherwise.
@@ -72,6 +81,8 @@ void write_cb(uv_write_t* wr_req, int status) {
     write_request_t *wr = (write_request_t*) wr_req;
     free(wr->buf.base);
     free(wr);
+
+    puts("Sent to server ! ");
 }
 
 void alloc_cb(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf) {
@@ -80,12 +91,16 @@ void alloc_cb(uv_handle_t* handle, size_t suggested_size, uv_buf_t* buf) {
 
 void read_cb(uv_stream_t* sock, ssize_t nread, const uv_buf_t* buf) {
     if (nread < 0) {
-        fprintf(stderr, "error echo_read");
-        uv_close((uv_handle_t *)sock, close_cb);
+        if (nread != UV_EOF) {
+            fprintf(stderr, "error read_cb");
+        } else {
+            fprintf(stderr, "EOF read_cb");
+        }
 
+        uv_close((uv_handle_t *)sock, 0); // 没资源需要回收，不需要注册 callback
         return;
     }
-    puts(buf->base);
+    printf("Receive from server: %s\n", buf->base);
 
     input_and_send(sock);
 }
